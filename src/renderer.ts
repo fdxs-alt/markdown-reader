916e9e23048c537a11c25c4f98b2c8182c0285a6
Jakub Sukiennik <kuba1207710@gmail.com>
2020-11-02 21:25:44 +0100

    adding opening file functionality

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,9 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import marked from "marked";
+import { remote, ipcRenderer } from 'electron'
+const { getFileFromUser } = remote.require('./main')
+import { basename } from 'path'
 
 window.onload = function () {
   
+  let filePath: string | null = null;
+  let originalContent = '';
+
+  const currentWindow = remote.getCurrentWindow();
+
   const openFileButton = document.querySelector('.open-file') as HTMLButtonElement;
   
   const saveFileButton = document.querySelector('.save-file') as HTMLButtonElement;
@@ -26,8 +34,49 @@ window.onload = function () {
     htmlView.innerHTML = marked(markdown);
   };
 
+  const updateUI = (isEdited?: boolean) => {
+    let title = 'Markdown Reader'
+
+    if(filePath) {
+      title = `${basename(filePath)} (Edited)`
+      currentWindow.setRepresentedFilename(filePath)
+    }
+
+    if(isEdited) {
+      title = `${title} - Edited`
+      currentWindow.setDocumentEdited(isEdited);
+    }
+
+    
+
+    saveFileButton.disabled = !isEdited;
+    revertButton.disabled = !isEdited;
+
+    currentWindow.setTitle(title);
+  }
+ 
   markdownView.addEventListener("keyup", (e) => {
     const currentConent = (e.target as HTMLTextAreaElement).value;
+
     renderHTML(currentConent);
+
+    updateUI(currentConent !== originalContent)
   });
+
+  openFileButton.addEventListener('click', (e) => {
+    getFileFromUser()
+  })
+
+  ipcRenderer.on('file-opened', (_, file: string, conent: string ) => {
+    filePath = file;
+
+    originalContent = conent;
+
+    markdownView.value = conent;
+    
+    renderHTML(conent);
+
+    updateUI()
+  })
 };
+
